49116875f0b55bc2971f5af54acf0ad9d6a18caf
Eric Gardner <gardner.ec@gmail.com>
2016-11-23 12:36:43 -0800

    Basic layout
    This commit sets up a basic layout (involving Neat grid w/flexbox and a
    basic ScrollMagic example) and adds some basic JS dependencies.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,27 @@
+/* eslint-disable */
 //= require_tree .
+/* eslint-enable */
+
+var ScrollMagic = ScrollMagic || {}
+
+$(function() { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  })
+
+  // get all slides
+  var slides = document.querySelectorAll('.section')
+
+  // create scene for every slide
+  for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i]
+    })
+      .setPin(slides[i])
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller)
+  }
+})
