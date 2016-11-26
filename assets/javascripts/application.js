3896093465fb744f95661276d89af68acd2ad45e
Eric Gardner <gardner.ec@gmail.com>
2016-11-25 16:20:57 -0800

    Automated commit at 2016-11-26 00:20:57 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -31,20 +31,20 @@ function drawCanvas() {
   var ctx = canvas.getContext('2d')
   var xMax = canvas.width = window.screen.availWidth
   var yMax = canvas.height = window.screen.availHeight
-  var hmTimes = Math.round(xMax + yMax)
+  var numberOfStars = Math.round(xMax + yMax)
 
-  for (var i = 0; i <= hmTimes; i++) {
+  for (var i = 0; i <= numberOfStars; i++) {
     var randomX = Math.floor((Math.random() * xMax) + 1)
     var randomY = Math.floor((Math.random() * yMax) + 1)
     var randomSize = Math.floor((Math.random() * 2) + 1)
     var randomOpacityOne = Math.floor((Math.random() * 9) + 1)
-    var randomOpacityTwo = Math.floor((Math.random() * 9) + 1)
+    var randomOpacityTwo = Math.floor((Math.random() * 2) + 1)
     var randomHue = Math.floor((Math.random() * 360) + 1)
     if (randomSize > 1) {
-      ctx.shadowBlur = Math.floor((Math.random() * 15) + 5)
+      ctx.shadowBlur = Math.floor((Math.random() * 2) + 5)
       ctx.shadowColor = 'white'
     }
-    ctx.fillStyle = 'hsla(' + randomHue + ', 30%, 80%, .' + randomOpacityOne + randomOpacityTwo + ')'
+    ctx.fillStyle = 'hsla(' + randomHue + ', 30%, 95%, .' + randomOpacityOne + randomOpacityTwo + ')'
     ctx.fillRect(randomX, randomY, randomSize, randomSize)
   }
 }
